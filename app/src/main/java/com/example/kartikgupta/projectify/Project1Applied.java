cda835f6948aa780b6903c18f94b12eccc72e518
matan <matanb@umich.edu>
2017-11-29 19:11:36 -0500

    UI design is IN!

---
+++ app/src/main/java/com/example/kartikgupta/projectify/Project1Applied.java
@@ -0,0 +1,13 @@
+package com.example.kartikgupta.projectify;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Project1Applied extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project1_applied);
+    }
+}
