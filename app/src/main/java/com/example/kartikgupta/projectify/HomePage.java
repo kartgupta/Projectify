902f5319cf45f0404bd1f237c4f15ee616af1b72
matan <matanb@umich.edu>
2017-12-03 21:39:11 -0500

    Fixing a few constraints

--- app/src/main/java/com/example/kartikgupta/projectify/HomePage.java
+++ app/src/main/java/com/example/kartikgupta/projectify/HomePage.java
@@ -2,12 +2,32 @@ package com.example.kartikgupta.projectify;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomePage extends Activity {
+public class HomePage extends Activity implements View.OnClickListener{
+
+    private Button buttonSave, buttonProjects, buttonMyProjects, buttonProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //buttonSave = (Button) findViewById(R.id.buttonSave);
+        //buttonProjects = (Button) findViewById(R.id.buttonProjects);
+        //buttonMyProjects = (Button) findViewById(R.id.buttonMyProjects);
+        //buttonProfile = (Button) findViewById(R.id.buttonProfile);
+
+        //buttonProfile.setOnClickListener(this);
+        //buttonMyProjects.setOnClickListener(this);
+        //buttonSave.setOnClickListener(this);
+        //buttonProjects.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/example/kartikgupta/projectify/MainActivity.java
+++ app/src/main/java/com/example/kartikgupta/projectify/MainActivity.java
@@ -1,16 +1,127 @@
 package com.example.kartikgupta.projectify;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 
+public class MainActivity extends Activity implements View.OnClickListener {
 
-public class MainActivity extends Activity {
+    private Button buttonLogin, buttonSignup;
+    private EditText editTextPassword, editTextUsername;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonSignup = (Button) findViewById(R.id.buttonSignup);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+
+        buttonLogin.setOnClickListener(this);
+        buttonSignup.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //catch value in edittext
+        String email = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        //figure out which button has been clicked
+        if (view == buttonLogin) {
+            //call the sigin method
+            signIn(email, password);
+        } else if (view == buttonSignup){
+            //call the create account method
+            createAccount(email, password);
+        }
+    }
+
+    public void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If signin fails, display a message to the user. If signin succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Registeration Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Registeration Sucessful",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+    public void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If signin fails, display a message to the user. If signin succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this,"Authentication Failed", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "authentication sucessful",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intentt = new Intent(MainActivity.this, HomePage.class);
+                            MainActivity.this.startActivity(intentt);
+                        }
+                    }
+                });
     }
 }
