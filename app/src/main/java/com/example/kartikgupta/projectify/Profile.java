258d3ff9b73627cda858ad78811b9830b073d6d7
Debby Hsu <yjhsu@umich.edu>
2017-12-06 11:55:06 -0500

    update profile page and add firebase connection to it

--- app/src/main/java/com/example/kartikgupta/projectify/Profile.java
+++ app/src/main/java/com/example/kartikgupta/projectify/Profile.java
@@ -1,13 +1,67 @@
 package com.example.kartikgupta.projectify;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Profile extends Activity {
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Profile extends Activity implements View.OnClickListener{
+
+    private EditText editTextSkill, editTextInterest, editTextExperience, editTextDesignation, editTextMoreInfo;
+    private Button buttonSaveProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        editTextSkill = findViewById(R.id.editTextSkill);
+        editTextInterest = findViewById(R.id.editTextInterest);
+        editTextExperience = findViewById(R.id.editTextExperience);
+        editTextDesignation = findViewById(R.id.editTextDesignation);
+        editTextMoreInfo = findViewById(R.id.editTextMoreInfo);
+        buttonSaveProfile = findViewById(R.id.buttonSaveProfile);
+
+        buttonSaveProfile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String skill = editTextSkill.getText().toString();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        final DatabaseReference profileRef = db.getReference(skill);
+
+        if(view == buttonSaveProfile) {
+            //user input data
+//            String TextSkill = editTextSkill.getText().toString();
+            String interest = editTextInterest.getText().toString();
+            String experience = editTextExperience.getText().toString();
+            String designation = editTextDesignation.getText().toString();
+            String moreInfo = editTextMoreInfo.getText().toString();
+
+            //add to db
+            user newProfile = new user(skill, interest, experience, designation, moreInfo);
+            profileRef.push().setValue(newProfile);
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/kartikgupta/projectify/user.java
@@ -0,0 +1,20 @@
+package com.example.kartikgupta.projectify;
+
+/**
+ * Created by debbyhsu on 12/6/17.
+ */
+
+public class user {
+    public String skill, interest, experience, designation, moreInfo;
+
+    public user() {
+    }
+
+    public user(String skill, String interest, String experience, String designation, String moreInfo) {
+        this.skill = skill;
+        this.interest = interest;
+        this.experience = experience;
+        this.designation = designation;
+        this.moreInfo = moreInfo;
+    }
+}
