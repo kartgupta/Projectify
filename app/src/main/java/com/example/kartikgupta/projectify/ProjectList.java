79bf4b0e03c4c796a1f4643edd224af624af543f
alim727 <alim727@gmail.com>
2017-12-08 19:21:48 -0500

    fixed projectlist to show both name and description of project. created custom Adapter.

--- app/src/main/java/com/example/kartikgupta/projectify/ProjectList.java
+++ app/src/main/java/com/example/kartikgupta/projectify/ProjectList.java
@@ -3,12 +3,17 @@ package com.example.kartikgupta.projectify;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
@@ -21,6 +26,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ProjectList extends Activity {
+    ArrayList<String> t1=new ArrayList<>();
+    ArrayList<String> d1=new ArrayList<>();
 
     private ListView listViewFindProjects;
     //add menu bar
@@ -33,11 +40,57 @@ public class ProjectList extends Activity {
     ArrayList<String> list = new ArrayList<>();
     ArrayAdapter<String> adapter;
 
+    class dataListAdapter extends BaseAdapter {
+        ArrayList<String> Title=new ArrayList<>();
+        ArrayList<String> Detail=new ArrayList<>();
+
+        dataListAdapter() {
+            Title = null;
+            Detail = null;
+        }
+
+        public dataListAdapter(ArrayList<String> text, ArrayList<String> text1) {
+            Title = text;
+            Detail = text1;
+        }
+
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return Title.size();
+        }
+
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View row;
+            row = inflater.inflate(R.layout.projectlayout, parent, false);
+            TextView textViewProjectName = (TextView) row.findViewById(R.id.textViewProjectName);
+            TextView textViewProjectDescription = (TextView) row.findViewById(R.id.textViewProjectDescription);
+            textViewProjectName.setText(Title.get(position));
+            textViewProjectDescription.setText(Detail.get(position));
+            return (row);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
         listViewFindProjects = findViewById(R.id.listViewFindProjects);
+        //new listViewFindProjects.setAdapter(new dataListAdapter(t1, d1));
+
+
+
 
         //menu bar
         buttonProfile = (Button) findViewById(R.id.buttonProfile);
@@ -87,11 +140,14 @@ public class ProjectList extends Activity {
 
         //menu bar end
 
-        adapter = new ArrayAdapter<String>(this, R.layout.projectlayout, R.id.textViewProjectName, list);
-        listViewFindProjects.setAdapter(adapter);
+       //prev adapter = new ArrayAdapter<String>(this, R.layout.projectlayout, R.id.textViewProjectName, list);
+        //prev listViewFindProjects.setAdapter(adapter);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference("Projects");
+         FirebaseDatabase database = FirebaseDatabase.getInstance();
+         final DatabaseReference myRef = database.getReference("Projects");
+
+        final dataListAdapter adapterNew = new dataListAdapter(t1,d1);
+        listViewFindProjects.setAdapter(adapterNew);
 
 
         myRef.addChildEventListener(new ChildEventListener() {
@@ -99,9 +155,12 @@ public class ProjectList extends Activity {
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Project project = new Project();
                 project = dataSnapshot.getValue(Project.class);
-                list.add(0, project.projectName);
+                //prev list.add(0, project.projectName);
                 //list.add(0, project.projectDescription);
-                adapter.notifyDataSetChanged();
+                //prev adapter.notifyDataSetChanged();
+                t1.add(0, project.projectName);
+                d1.add(0, project.projectDescription);
+                adapterNew.notifyDataSetChanged();
             }
 
             @Override
@@ -130,7 +189,7 @@ public class ProjectList extends Activity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //Toast.makeText(ProjectList.this, "text"+position, Toast.LENGTH_SHORT).show();
                 int position = i;
-                String value = listViewFindProjects.getItemAtPosition(position).toString();
+                String value = ((TextView) view.findViewById(R.id.textViewProjectName)).getText().toString();
                 //Toast.makeText(ProjectList.this, "text"+value, Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(ProjectList.this, Project1.class);
